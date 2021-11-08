47122d9a61676ab4c1a58e2bea32cbec0ce40e3e
Enzotlo <tueleauenzo@gmail.com>
2021-11-08 15:04:50 +0100

    Exo 1 : Routes, controleur de contact, menu de nav

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ContactController extends AbstractController
+{
+    /**
+     * @Route("/contact", name="contact")
+     */
+    public function index(): Response
+    {
+        return $this->render('contact/index.html.twig', [
+        ]);
+    }
+    /**
+     * @Route("/contact/{type}", name="contactType")
+     */
+    public function ContactTypes(Request $request, string $type): Response
+    {
+        $name = $request->query->get('name');
+        return $this->render('contact/index.html.twig', [
+            'name' => $name,
+            'type' => $type
+        ]);
+    }
+}
